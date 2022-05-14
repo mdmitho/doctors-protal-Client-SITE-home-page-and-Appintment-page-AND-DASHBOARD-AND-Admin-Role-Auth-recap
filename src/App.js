2b26034aabfd1d19ffa52332600f4b7cad4d2876
Hossen <mdmitho199911@gmail.com>
2022-05-14 23:51:13 +0600

    Save Registered user information in the database

---
+++ src/App.js
@@ -0,0 +1,53 @@
+
+import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
+import Appointemnt from './Pages/Appointment/Appointemnt';
+import Signup from './Pages/Signup/Signup';
+import RequireAuth from './Pages/RequireAuth/RequireAuth';
+import { Toaster } from "react-hot-toast";
+import Dashboard from './Pages/Dashboard/Dashboard';
+import MyAppointments from './Pages/Dashboard/MyAppointments/MyAppointments';
+import Review from './Pages/Review/Review';
+import MyReview from './Pages/Dashboard/MyReview/MyReview';
+import MyHistory from './Pages/Dashboard/MyHistory/MyHistory';
+
+function App() {
+  return (
+    <div className=" max-w-7xl mx-auto bg-white">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/appointment"
+          element={
+            <RequireAuth>
+              <Appointemnt />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <RequireAuth>
+              <Dashboard />
+            </RequireAuth>
+          }
+        >
+          <Route index element={<MyAppointments></MyAppointments>}></Route>
+          <Route path="review" element={<MyReview></MyReview>}></Route>
+          <Route path="history" element={<MyHistory></MyHistory>}></Route>
+        </Route>
+      </Routes>
+      <Toaster />
+    </div>
+  );
+}
+
+export default App;
