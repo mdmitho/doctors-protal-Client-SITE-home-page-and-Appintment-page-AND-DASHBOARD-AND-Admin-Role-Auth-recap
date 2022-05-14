4c478061f1f3d7786d33262b71028dc5e2279f27
Hossen <mdmitho199911@gmail.com>
2022-05-15 03:59:48 +0600

    Prevent accessing Admin route via URL

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Review from './Pages/Review/Review';
 import MyReview from './Pages/Dashboard/MyReview/MyReview';
 import MyHistory from './Pages/Dashboard/MyHistory/MyHistory';
 import Users from './Pages/Dashboard/Users/Users';
+import RequireAdmin from './Pages/RequireAdmin/RequireAdmin';
 
 function App() {
   return (
@@ -44,7 +45,14 @@ function App() {
           <Route index element={<MyAppointments></MyAppointments>}></Route>
           <Route path="review" element={<MyReview></MyReview>}></Route>
           <Route path="history" element={<MyHistory></MyHistory>}></Route>
-          <Route path="users" element={<Users></Users>}></Route>
+          <Route
+            path="users"
+            element={
+              <RequireAdmin>
+                <Users></Users>
+              </RequireAdmin>
+            }
+          ></Route>
         </Route>
       </Routes>
       <Toaster />
